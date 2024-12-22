a27cc38ec2ee612e45ab41ce72c9ed6ca962f213
Shanti Rai <shantirai@MacBookAir.lan>
2024-12-22 20:18:49 +0000

    Finished and tested getGameByTitle() and the .get() request to read and return the valid data or return error object.

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-import { readData } from "/Users/shantirai/Desktop/Projects/Practice_back-end/RESTful_project1/helperFunctions.js";
+import {
+  readData,
+  getGameByTitle,
+} from "/Users/shantirai/Desktop/Projects/Practice_back-end/RESTful_project1/helperFunctions.js";
 import express from "express";
 
 // Create an instance of an Express application
@@ -9,7 +12,7 @@ app.use(express.json());
 
 const PORT = 3000;
 
-//Task 1 - Get all astronauts
+//Task 1 - Get all data
 // write a get reuest to endpoint and get all data
 //Add an endpoint to your REST API which returns all astronauts in the response body.
 // set up try catch block
@@ -31,8 +34,52 @@ app.get("/", async function (req, res) {
   }
 });
 
+//## Task 2 - Get a particular set of data AND add an endpoint to your REST API which returns a particular dataset in the response body- get back by name
+// write a get request to endpoint - /:id or /:name
+// set up try catch block
+// call the right helper function and save the output to a variable
+// return the res.json(saidvariabe)
+
+app.get("/:name", async function (req, res) {
+  try {
+    // save the request to a variable
+    const { name } = req.params;
+
+    const gameDataByName = await getGameByTitle(name);
+
+    // Check if gameDataByName is an error message or valid data
+    if (gameDataByName === "Error, please check input.") {
+      return res.status(404).json({
+        success: false,
+        message: "Game not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      payload: gameDataByName,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch data",
+    });
+  }
+});
+
 // Start the server and listen on the defined port
 app.listen(PORT, () => {
   // Log a message to the console when the server is successfully running
   console.log(`App listening at http://localhost:${PORT}`);
 });
+
+/* [
+  'Galactic Bowling',
+  'Train Bandit',
+  'Jolt Project',
+  'Henosis',
+  'Two Weeks in Painland',
+  'Kooring VR Coding Adventure',
+  'Anarchy',
+  'Dark Throne'
+]*/
--- helperFunctions.js
+++ helperFunctions.js
@@ -1,18 +1,64 @@
-import { read } from "fs";
 import fs from "fs/promises";
 
+// recreate a copy od the data so the original is preserved.
+
+// function to read and return all data
 export async function readData() {
   try {
     const fileData = await fs.readFile(
       "/Users/shantirai/Desktop/Projects/Practice_back-end/RESTful_project1/games.json",
       "utf8"
     );
+    // convert a JSON string into a JS object
     const jsonData = JSON.parse(fileData);
-    return jsonData;
+
+    // Return an array of the key/value pairs of an object as an array is easier to manage
+    const allData = Object.entries(jsonData);
+
+    return allData;
   } catch (error) {
     console.error("ERROR", error);
   }
 }
 
-const allData = await readData();
-console.log(allData); //prints Promise { <pending> }
+const data = await readData();
+
+// returns an array with all game titles
+function getAllGameTitles() {
+  let titles = [];
+  for (let key in data) {
+    if (data.hasOwnProperty(key)) {
+      console.log(key, data[key][0]);
+      titles.push(data[key][0]);
+    }
+  }
+  return titles;
+}
+console.log(getAllGameTitles());
+
+//## Task 2 - Get a particular set of data AND add an endpoint to your REST API which returns a particular dataset in the response body- get back by name
+
+export async function getGameByTitle(title) {
+  // create vraibles lowercase
+  title = title.toLowerCase().trim();
+
+  // Loop through the array of games
+  for (let i = 0; i < data.length; i++) {
+    // for readability save the name value in its own variable
+    let gameName = data[i][0];
+    //sanitize data- remove whitespaces and make lowercase
+    let sanitized = gameName.replace(/\s+/g, "").toLowerCase();
+
+    // check all entries in the data array for lowerCase() matches
+    if (sanitized === title) {
+      // if match found, return that isolated object
+
+      return data[i];
+    }
+  }
+
+  // else return no object found with that title
+  return "Error, please check input.";
+}
+
+console.log(getGameByTitle("TwoWeeksinPainland"));
